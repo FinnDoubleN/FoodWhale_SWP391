8221d840a2bd640385655f6870540f02e64f3de5
TrầnHiệpQuốcHE150051 <91178389+TranHiepQuocHE150051@users.noreply.github.com>
2021-11-30 18:25:21 +0700

    update auth

--- src/java/dal/DAOauth.java
+++ src/java/dal/DAOauth.java
@@ -5,10 +5,25 @@
  */
 package dal;
 
+import java.util.regex.Pattern;
+
 /**
  *
  * @author This PC
  */
 public class DAOauth {
-    
+    public boolean AdAuth(String user, String pattern){
+        Pattern p = Pattern.compile(pattern);
+        //[a-zA-Z][a-zA-Z0-9]+@[a] -> admin
+        //[a-zA-Z][a-zA-Z0-9]+@[m] ->manager
+        //[a-zA-Z][a-zA-Z0-9]+@[s] ->staff
+        if(p.matcher(user).find()){
+            return true;
+        }
+        return false;
+        //if return false -> user
+    }
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("user.dir"));
+    }
 }
