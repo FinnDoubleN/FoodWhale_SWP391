bcb6fb1b7e64e9046f7cb7beb24dba8c8f5b20cb
Giap <milo20820@gmail.com>
2021-12-01 15:12:33 +0700

    fix

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -19,7 +19,7 @@ public class DBContext {
         try {
             //Change the username password and url to connect your own database
             String username = "sa";
-            String password = "sa";
+            String password = "123456";
             String url = "jdbc:sqlserver://localhost:1433;databaseName=FoodWhale";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, username, password);
