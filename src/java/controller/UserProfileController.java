d17e56b26f9cae2386870283a84f5c13a1abf039
anhminh1241 <73695612+anhminh1241@users.noreply.github.com>
2021-12-02 21:04:02 +0700

    listRecipe

--- src/java/controller/UserProfileController.java
+++ src/java/controller/UserProfileController.java
@@ -5,6 +5,7 @@
  */
 package controller;
 
+<<<<<<< HEAD
 import dal.FoodWhaleDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -16,12 +17,25 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+=======
+import DAO.UserDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+>>>>>>> origin/HoangAnh
 import model.User;
 
 /**
  *
  * @author ADMIN
  */
+<<<<<<< HEAD
 
 public class UserProfileController extends HttpServlet {
 
@@ -39,6 +53,10 @@ public class UserProfileController extends HttpServlet {
         return null;
     }
 
+=======
+public class UserProfileController extends HttpServlet {
+
+>>>>>>> origin/HoangAnh
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -47,6 +65,7 @@ public class UserProfileController extends HttpServlet {
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
+<<<<<<< HEAD
      * @throws java.sql.SQLException
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
@@ -70,6 +89,28 @@ public class UserProfileController extends HttpServlet {
             }
         }
     }
+=======
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+        User loggedin;
+            UserDAO getU = new UserDAO();
+            HttpSession session = request.getSession();
+        String email = (String) session.getAttribute("email");
+                //get user object
+                 User user = getU.getUserByEmail(email);
+             request.setAttribute("data", user);
+        
+        request.getRequestDispatcher("Profile.jsp").forward(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(UserProfileController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       
+        }
+    
+>>>>>>> origin/HoangAnh
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -83,11 +124,15 @@ public class UserProfileController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+<<<<<<< HEAD
         try {
             processRequest(request, response);
         } catch (SQLException ex) {
             Logger.getLogger(UserProfileController.class.getName()).log(Level.SEVERE, null, ex);
         }
+=======
+        processRequest(request, response);
+>>>>>>> origin/HoangAnh
     }
 
     /**
@@ -101,11 +146,15 @@ public class UserProfileController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+<<<<<<< HEAD
         try {
             processRequest(request, response);
         } catch (SQLException ex) {
             Logger.getLogger(UserProfileController.class.getName()).log(Level.SEVERE, null, ex);
         }
+=======
+        processRequest(request, response);
+>>>>>>> origin/HoangAnh
     }
 
     /**
