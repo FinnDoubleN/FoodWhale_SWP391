c4af7e309c695a44a13f116019e325b0df8840c7
DannyL999 <80804243+DannyL999@users.noreply.github.com>
2021-12-06 17:04:10 +0700

    Add files via upload

--- src/java/dal/UserDAO.java
+++ src/java/dal/UserDAO.java
@@ -5,6 +5,7 @@
  */
 package dal;
 
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.logging.Level;
@@ -16,7 +17,22 @@ import model.User;
  * @author ADMIN
  */
 public class UserDAO extends MyDAO {
-
+ public void EditUser(User u) {
+        try {
+            String sql = "update [FoodWhale].[dbo].[User] set Email=?, ,uName=?,Image=?,Gender=?,Address=?,Phone=? where uID=?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, u.getEmail());
+            statement.setString(2, u.getUsername());
+            statement.setString(3, u.getImage());
+            statement.setString(4, u.getGender());
+            statement.setString(5, u.getAddress());
+            statement.setString(6, u.getPhone());
+            statement.setInt(17, u.getuID());
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     public User getUserByEmail(String email) throws Exception {
         User user = new User();
         ArrayList<User> lu = new ArrayList<>();
@@ -25,6 +41,33 @@ public class UserDAO extends MyDAO {
             if (con != null) {
                 ps = con.prepareStatement(xsql);
                 ps.setString(1, email);
+                rs = ps.executeQuery();
+                while (rs.next()) {
+                    user.setuID(rs.getInt(1));
+                    user.setEmail(rs.getString(2));
+                    user.setPassword(rs.getString(3));
+                    user.setUsername(rs.getString(4));
+                    user.setImage(rs.getString(5));
+                    user.setDate(rs.getDate(6));
+                    user.setGender(rs.getString(7));
+                    user.setAddress(rs.getString(8));
+                    user.setPhone(rs.getString(9));
+                    user.setRole(rs.getString(10));
+                    System.out.println(user);
+                }
+            }
+        } catch (SQLException e) {
+        }
+        return user;
+    }
+    
+     public User getUserByID(int uID) throws Exception {
+        User user = new User();
+        String xsql = "select * from [FoodWhale].[dbo].[User] where uID= ?";
+        try {
+            if (connection != null) {
+                ps = connection.prepareStatement(xsql);
+                ps.setInt(1, uID);
                 rs = ps.executeQuery();
                 while (rs.next()) {
                     user.setuID(rs.getInt(1));
